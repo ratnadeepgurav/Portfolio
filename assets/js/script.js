1f73b0c233b0c2b368d497b91cbbee8a73cf3847
Ratnadeep Gurav <deepgurav2329@gmail.com>
2025-11-11 09:25:05 +0530

    remove chatbot

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,15 +185,15 @@ document.onkeydown = function (e) {
 };
 
 // ========== TAWK.TO LIVE CHAT ==========
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
+// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+// (function () {
+//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+//     s1.async = true;
+//     s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+//     s1.charset = 'UTF-8';
+//     s1.setAttribute('crossorigin', '*');
+//     s0.parentNode.insertBefore(s1, s0);
+// })();
 
 // ========== SCROLL REVEAL ANIMATIONS ==========
 const srtop = ScrollReveal({
